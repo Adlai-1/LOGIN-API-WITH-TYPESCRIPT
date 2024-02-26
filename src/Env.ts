09c045f94f145ea9ebbf6ee829d754a07e60cfac
Adlai-1 <netteydavid8@gmail.com>
2024-02-26 13:17:46 +0000

    Env.ts changes made

--- src/Env.ts
+++ src/Env.ts
@@ -7,11 +7,7 @@ interface ENV {
 
 export const ENVs: ENV = {
     PORT: 3000,
-    MONGO: "mongodb://127.0.0.1:27017/Auth",
-    SECRETKEY: "5437d5ad1411d437d31dc5a395aec2568c231662accfd24b24d8cbdd086c45ef"
+    MONGO: "Your own MONGODB URI",
+    SECRETKEY: "Your own JWT SecretKey"
 }
 
-/**
-MONGO: "mongodb://127.0.0.1:27017/Auth",
-SECRETKEY: "5437d5ad1411d437d31dc5a395aec2568c231662accfd24b24d8cbdd086c45ef"
-**/
